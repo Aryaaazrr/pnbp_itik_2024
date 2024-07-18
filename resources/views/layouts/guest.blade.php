1924e701b99c36de6d182c04f15c8a56faa21047
StYulia <94944128+Aryaaazrr@users.noreply.github.com>
2024-07-18 18:23:09 +0700

    fe login

---
+++ resources/views/layouts/guest.blade.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Font cdn link js -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+    <Title>Login | Tosepatu - Anda Untung Kami Berkah</Title>
+
+    <!-- Link eksternal file CSS -->
+    {{-- <link rel="stylesheet" href="css/style.css" /> --}}
+
+    <!-- Icon -->
+    <link rel="shortcut icon" href="/img/icon-tab.jpg" />
+
+    <!-- Font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap"
+        rel="stylesheet" />
+        <link href="https://cdn.jsdelivr.net/npm/flowbite@2.4.1/dist/flowbite.min.css" rel="stylesheet" />
+    @vite('resources/css/app.css')
+</head>
+
+<body class="bg-secondary">
+    {{-- <h1 class="text-3xl font-bold underline"> --}}
+    <div class="w-full flex min-h-screen items-center justify-center">
+        @yield('content')
+    </div>
+    {{-- </h1> --}}
+    <script src="https://cdn.jsdelivr.net/npm/flowbite@2.4.1/dist/flowbite.min.js"></script>
+</body>
+
+</html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,42 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
     content: [
-        "./resources/**/*.blade.php",
-        "./resources/**/*.js",
-        "./resources/**/*.vue",
+      "./resources/**/*.blade.php",
+      "./resources/**/*.js",
+      "./resources/**/*.vue",
+      "./node_modules/flowbite/**/*.js"
     ],
     theme: {
-        extend: {},
+      extend: {
+        container: {
+          center: true,
+          padding: '16px',
+        },
+        colors: {
+          primary: '#62341F',
+          secondary: '#C3AD72',
+          hover: '#7E3F22',
+          dark: '#18181b',
+          navy: '#2E417B',
+          tosca: 'rgba(95, 211, 208, 0.15)',
+          grey: '#F6F6F6'
+        },
+        keyframes: {
+          wiggle: {
+            '0%, 100%': { transform: 'translateY(0rem)' },
+            '50%': { transform: 'translateY(2.3rem)' },
+          }
+        },
+        animation: {
+          wiggle: 'wiggle 3s linear infinite',
+        },
+        dropShadow: {
+          '2xl' : '0px 10px 50px rgba(95, 211, 208, 0.20)'
+        }
+      },
     },
-    plugins: [],
-};
+    plugins: [
+      require('flowbite/plugin')
+    ],
+  }
+
