586a7a1209b2af01d9da29f2a80ecada010aacdd
StYulia <94944128+Aryaaazrr@users.noreply.github.com>
2024-08-21 14:57:52 +0700

    convert vite-mix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap"
         rel="stylesheet" />
     <link href="https://cdn.jsdelivr.net/npm/flowbite@2.4.1/dist/flowbite.min.css" rel="stylesheet" />
-    @vite('resources/css/app.css')
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 
 <body class="bg-secondary">
@@ -27,11 +27,11 @@
             @include('partials.app.navbar')
             @include('partials.app.sidebar')
             {{-- <h1 class="text-3xl font-bold underline"> --}}
-                {{-- </h1> --}}
-                @yield('content')
-                @include('partials.app.footer')
-            </main>
-        </div>
+            {{-- </h1> --}}
+            @yield('content')
+            @include('partials.app.footer')
+        </main>
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/flowbite@2.4.1/dist/flowbite.min.js"></script>
 
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -23,7 +23,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap"
         rel="stylesheet" />
     <link href="https://cdn.jsdelivr.net/npm/flowbite@2.4.1/dist/flowbite.min.css" rel="stylesheet" />
-    @vite('resources/css/app.css')
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 
 <body>
